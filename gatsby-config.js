864e2137bc87eb8dbb9cafc9e76db489bf1f37b5
João Paulo <joaopaulogse@gmail.com>
2020-05-13 14:46:22 -0300

    add repo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  pathPrefix: "/joaopaulogse",
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Full Stack Developer João Paulo`,
+    description: `Node.js, TypeScript, React, Vue, Docker, Kubernetes, Microservices`,
+    author: `@joaopaulogse`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
